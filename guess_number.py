2e93c77ede6dae623226172cda1d93ec78bf9a3c
Lance Kao <717126@gmail,com>
2019-05-25 10:22:01 +0800

    add counter

--- guess_number.py
+++ guess_number.py
@@ -1,8 +1,10 @@
 import random
 
 ran_num = random.randint(1,100)
+count = 0
 
 while True:
+	count += 1
 	guess_number = input('請輸入數字: ')
 	guess_number = int(guess_number)
 	if guess_number < ran_num:
@@ -11,4 +13,6 @@ while True:
 		print('比答案大')
 	else:
 		print('你答對了答案就是: ', ran_num)
+		print('一共猜了', count, '次')
 		break
+	print('一共猜了', count, '次')

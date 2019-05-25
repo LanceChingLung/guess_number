242d6d9576942ded254f0fbdaec2fb86947f9305
Lance Kao <717126@gmail,com>
2019-05-25 10:28:24 +0800

    update number for user setting range

--- guess_number.py
+++ guess_number.py
@@ -1,6 +1,10 @@
 import random
 
-ran_num = random.randint(1,100)
+max_num = input('請輸入最大範圍數字: ')
+max_num = int(max_num)
+min_num = input('請輸入最小範圍數字: ')
+min_num = int(min_num)
+ran_num = random.randint(min_num, max_num)
 count = 0
 
 while True:
